75f815ebd999e7fbd63544d302e8c01c83314d8c
priyadharshin956-art <priyadharshin956@gmail.com>
2026-07-03 18:14:10 +0530

    Add files via upload

---
+++ Stock Portfolio Tracker.py
@@ -0,0 +1,23 @@
+# Stock Portfolio Tracker
+
+stocks = {
+    "AAPL": 180,
+    "TSLA": 250,
+    "GOOGL": 150,
+    "MSFT": 300
+}
+
+total = 0
+
+n = int(input("Enter number of stocks: "))
+
+for i in range(n):
+    stock = input("Enter stock name: ").upper()
+    quantity = int(input("Enter quantity: "))
+
+    if stock in stocks:
+        total += stocks[stock] * quantity
+    else:
+        print("Stock not found!")
+
+print("\nTotal Investment Value = $", total)
